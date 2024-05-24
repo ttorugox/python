8b6ffbe1ab832aad311d1068308180264a2558f1
ttorugox <vitorcaim9@gmail.com>
2024-05-24 17:06:51 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -1,1 +1,13 @@
-
+while True:
+  numeros = []
+  for x in range(4):
+    print("Digite o numero desejado")
+    
+    num = float(input())
+    numero.append(num)
+    if num == 0:
+      break
+  print("Voce digitou os nº: , numero")
+  if num == 0:
+    break
+print("Programa Finalizado")
